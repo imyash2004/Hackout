a268c3cf2d576628487a790962b8187aead70833
imyash2004 <agarwalaysh041@gmail.com>
2023-11-04 22:23:44 +0530

    video uploading

--- src/main/java/com/example/hack1/Controller/HomeController.java
+++ src/main/java/com/example/hack1/Controller/HomeController.java
@@ -3,11 +3,22 @@ package com.example.hack1.Controller;
 import com.example.hack1.Repository.UserRepo;
 import com.example.hack1.entities.Message;
 import com.example.hack1.entities.User;
+import com.example.hack1.entities.Video;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.Principal;
 
 @Controller
 public class HomeController
@@ -58,14 +69,64 @@ public class HomeController
     public String login1(){
         return "login";
     }
-    @GetMapping("/dologin")
-    public String login(@RequestParam("email") String email,@RequestParam("password") String password){
+    @PostMapping("/dologin")
+    public String login(@RequestParam("email") String email,@RequestParam("password") String password,HttpSession session){
         User u=this.userRepo.getUserByEmail(email);
         if(u.getPassword().equals(password)){
             return"home";
         }
         else {
         return "login";
-    }}
+    }
+    }
+    @GetMapping("/upload")
+    public String upload(){
+        return "upload";
+    }
+    @PostMapping("/uploading")
+    public String uploaded(@ModelAttribute Video video, Principal p, @RequestParam("vrl") MultipartFile vr,HttpSession session){
+        try {
+            String name=p.getName();
+            System.out.println(name);
+            User user=this.userRepo.getUserByEmail(name);
+            video.setUserId(user.getUserId());
+
+
+
+            if(vr.isEmpty()){
+
+                System.out.println("file is empty");
+                return "upload";
+            }
+            else{
+                video.setVideoUrl(vr.getOriginalFilename() );
+                File file=new ClassPathResource("static/video").getFile();
+                Path path = Paths.get(file.getAbsolutePath()+File.separator+vr.getOriginalFilename());
+
+                Files.copy(vr.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("video is uploaded");
+            }
+            video.setTags(null);
+            video.setComments(null);
+            video.setDislikes(0);
+            video.setLikes(0);
+
+
+
+
+            session.setAttribute("message",new Message("your contact is added successfully","success"));
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("error"+e.getMessage());
+
+            session.setAttribute("message",new Message("something went wrong try again","alert"));
+
+        }
+        return "home";
+        }
+
 
 }
---
+++ src/main/java/com/example/hack1/entities/Video.java
@@ -0,0 +1,136 @@
+package com.example.hack1.entities;
+
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
+
+@Entity
+@Table(name="Video")
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer videoId;
+    private String description;
+    private String title;
+    @ElementCollection
+    private Set<String> tags;
+    @ElementCollection
+    private ArrayList<String> comments;
+    private Integer userId;
+    private Integer likes;
+    private Integer dislikes;
+    private String VideoUrl;
+
+    public Integer getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(Integer videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<String> comments) {
+        this.comments = comments;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Integer dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public String getVideoUrl() {
+        return VideoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        VideoUrl = videoUrl;
+    }
+
+    public Video(Integer videoId, String description, Set<String> tags, ArrayList<String> comments, Integer likes, Integer dislikes, String videoUrl) {
+        this.videoId = videoId;
+        this.description = description;
+        this.tags = tags;
+        this.comments = comments;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        VideoUrl = videoUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "videoId=" + videoId +
+                ", description='" + description + '\'' +
+                ", tags=" + tags +
+                ", comments=" + comments +
+                ", likes=" + likes +
+                ", dislikes=" + dislikes +
+                ", VideoUrl='" + VideoUrl + '\'' +
+                '}';
+    }
+
+    public Video() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Video(Integer videoId, String description, String title, Set<String> tags, ArrayList<String> comments, Integer userId, Integer likes, Integer dislikes, String videoUrl) {
+        this.videoId = videoId;
+        this.description = description;
+        this.title = title;
+        this.tags = tags;
+        this.comments = comments;
+        this.userId = userId;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        VideoUrl = videoUrl;
+    }
+}
